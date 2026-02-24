f3d0878db6ae732c302ced4deba6f93aaaa2eb93
mohitfonix74 <mohit.kumawat@Fonixtech.io>
2026-02-24 13:42:12 +0530

    fix: correct case-sensitive import for bookFreeTrialController (Linux fix)

--- src/routes/bookFreeTrialRoute.js
+++ src/routes/bookFreeTrialRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/BookFreeTrialController');
+const controller = require('../controllers/bookFreeTrialController');
 
 router.get('/getform', controller.getAllBookFreeTrials);
 router.put('/:id', controller.updateBookFreeTrial);
